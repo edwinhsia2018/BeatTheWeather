ef7a4bf369b1ece62ba92e2ff88ca93068d22bdd
Edwin Hsia <edwinhsia@gmail.com>
2018-03-22 13:03:17 -0500

    3.22 1:03 updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 function getWeather(){
+    zipcode = $("#zip")
     var queryURL = "weather url" + currentWeather + "";
     $.ajax({
         url: queryURL,
@@ -10,17 +11,21 @@ function getWeather(){
     })
 }
 
-if (currentWeather = sunny && temperature > 60) {
-    var suggestedFood = "icecream"
+if (currentWeather = sunny) {
+    var suggestedFood = "icecream";
+    $("#weather-background").attr("src", "https://i.pinimg.com/originals/92/39/54/92395443ed69dbdd163988ae680874df.jpg");
 }
 else if (currentWeather = rainy) {
 
+    $("#weather-background").attr("src", "http://kb4images.com/images/rain-images/38093866-rain-images.jpg");
 }
 else if (currentWeather = snowing) {
 
+    $("#weather-background").attr("src", "https://wallpaperscraft.com/image/trees_winter_blizzard_wind_snow_14899_3840x2160.jpg");
 }
 else if (currentWeather = windy) {
 
+    $("#weather-background").attr("src", "http://bsnscb.com/data/out/23/38783249-blow-wallpapers.jpg");
 }
 
 
