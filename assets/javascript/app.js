feee8b951a497735bd6cbe515566a87481c96e47
Edwin Hsia <edwinhsia@gmail.com>
2018-03-22 13:09:31 -0500

    3.22 1:09 updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,25 +8,27 @@ function getWeather(){
     .then(function(response){
         var currentWeather = response.data.currentWeather;
         $("#weather").append(response.currentWeather);
+
+        if (currentWeather = sunny) {
+            var suggestedFood = "icecream and smoothies";
+            $("#weather-background").attr("src", "https://i.pinimg.com/originals/92/39/54/92395443ed69dbdd163988ae680874df.jpg");
+        }
+        else if (currentWeather = rainy) {
+            var suggestedFood = "";
+            $("#weather-background").attr("src", "http://kb4images.com/images/rain-images/38093866-rain-images.jpg");
+        }
+        else if (currentWeather = snowing) {
+            var suggestedFood = "soups";
+            $("#weather-background").attr("src", "https://wallpaperscraft.com/image/trees_winter_blizzard_wind_snow_14899_3840x2160.jpg");
+        }
+        else if (currentWeather = windy) {
+            var suggestedFood = "";
+            $("#weather-background").attr("src", "http://bsnscb.com/data/out/23/38783249-blow-wallpapers.jpg");
+        }
     })
 }
 
-if (currentWeather = sunny) {
-    var suggestedFood = "icecream";
-    $("#weather-background").attr("src", "https://i.pinimg.com/originals/92/39/54/92395443ed69dbdd163988ae680874df.jpg");
-}
-else if (currentWeather = rainy) {
 
-    $("#weather-background").attr("src", "http://kb4images.com/images/rain-images/38093866-rain-images.jpg");
-}
-else if (currentWeather = snowing) {
-
-    $("#weather-background").attr("src", "https://wallpaperscraft.com/image/trees_winter_blizzard_wind_snow_14899_3840x2160.jpg");
-}
-else if (currentWeather = windy) {
-
-    $("#weather-background").attr("src", "http://bsnscb.com/data/out/23/38783249-blow-wallpapers.jpg");
-}
 
 
 function displayFood() {
