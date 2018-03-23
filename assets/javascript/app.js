73a623d2f3f17128321283dcdf5b4f3a39c3aeaa
Jayson <jaysonchen64@gmail.com>
2018-03-23 10:09:51 -0500

    weather

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,20 @@
+$("#submitW").on("click", function(event){
+    event.preventDefault();
+    var apiKey = "d23506797d07737660408148e7533f22"
+    var zip = $("#zipCodeForm").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid="+ apiKey;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function(response){
+        console.log(queryURL)
+        console.log(response)
+        $(".display-4").text(response.main.temp);        
+    })
+})
+
 function getWeather(){
     zipcode = $("#zip")
     var queryURL = "weather url" + currentWeather + "";
@@ -46,3 +63,4 @@ function displayFood() {
         }
     })
 }
+
